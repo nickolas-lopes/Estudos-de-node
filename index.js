8737a35f4f6ccc95e929496cbd5d2033dbc9231c
Nickolas <nickolaslopes.developer@gmail.com>
2025-07-16 00:19:56 -0300

    feat: add get and post methods

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require("express")
-const server = express()
-server.get("/hello", (req, res) =>{
-    const nome = req.query.nome
-    return res.json({
-        title : "Hello World !",
-        message : `Olá ${nome}, tudo bem ?`
-    })
-})
-server.listen(3000)
+const express = require("express");
+const server = express();
+server.use(express.json());
+let customers = [
+  { id: 1, name: "user1" },
+  { id: 2, name: "user2" },
+  { id: 3, name: "user3" },
+];
+
+server.get("/customers", (req, res) => {
+  return res.json(customers);
+});
+
+server.get("/customers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const customer = customers.find((item) => item.id === id);
+  const status = customer ? 200 : 404;
+  return res.status(status).json(customer);
+});
+
+server.post("/customers", (req, res) => {
+  const {name} = req.body;
+  const id = customers[customers.length - 1].id + 1;
+  const newCostumer = { id, name };
+  customers.push(newCostumer);
+  return res.status(201).json(newCostumer);
+});
+server.listen(3000);
